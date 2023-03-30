87de7dbc217c3958d005e5155637f90483ed72a3
Luigi160397 <luigi.difraia.97@gmail.com>
2023-03-30 13:29:36 +0200

    esercizio base da ieri

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import logo from "./logo.svg";
 import "./App.css";
+import AllTheBooks from "./components/AllTheBooks";
+import MyFooter from "./components/MyFooter";
+import MyNavbar from "./components/MyNavbar";
+import Welcome from "./components/Welcome";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+      <MyNavbar />
+      <Welcome />
+      <AllTheBooks />
+      <MyFooter />
     </div>
   );
 }
